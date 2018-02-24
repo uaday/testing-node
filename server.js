4f660d6e2e4a8d88cfd5a7bc8a7ec1f70e267acb
uaday <uadayghosh@gmail.com>
2018-02-24 23:16:20 +0600

    testing node-app

---
+++ server.js
@@ -0,0 +1,19 @@
+const express= require('express');
+const hbs= require('hbs');
+const port=process.env.PORT||3000;
+
+var app= express();
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/public'));
+
+app.get('/',(req,res)=>{
+  res.send('<h1>Hello Sudipta</h1>');
+});
+app.get('/login',(req,res)=>{
+  res.render('login.hbs',{
+    title:'login-page'
+  })
+});
+app.listen(port,()=>{
+  console.log(`The app is running on ${port}`);
+});
